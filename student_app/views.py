21879e64110c7d37061ff34dbaa83b0f758de070
veeraganeshbabu <veeraganeshkarravula@gmail.com>
2022-07-15 11:52:32 +0530

    Initial commit

---
+++ student_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+# Create your views here.
+class Home(View):
+    def get(self,request):
+        return render(request,'home.html')
+
